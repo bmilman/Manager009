df89a15c2e2fe8e6c146b3e4fc13e3c17a91273b
bernard milman <gkjhkj@yhioyhik.com>
2026-01-03 12:03:31 -0500

    Initial Commit

---
+++ Manager009/Manager009App.swift
@@ -0,0 +1,17 @@
+//
+//  Manager009App.swift
+//  Manager009
+//
+//  Created by bernard milman on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Manager009App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
